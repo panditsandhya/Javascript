d1e3670d5a3b7b89bd46bd6f3f89de7397bd3127
Sandhya <144099442+panditsandhya@users.noreply.github.com>
2023-09-05 13:31:57 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,38 @@
+//let score = "90abc"
+//let score = null
+// let score = undefined
+// let score = true
+// let score = false
+let score = "sandhya"
+// console.log(typeof score)
+// console.log(typeof (score))
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber) 
+// console.log(valueInNumber)
+
+
+//"33" => 33
+//"33abc" => NaN
+//true => 1; false => 0
+
+
+// let isLoggedIn = 1
+// let isLoggedIn = 0
+//let isLoggedIn = ""
+// let isLoggedIn = null
+// let isLoggedIn = "sandhya"
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+
+//1 => true; 0 => false
+//"" => false
+//"sandhya" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
