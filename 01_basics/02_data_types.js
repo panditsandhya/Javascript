595adc75664603ee33270b2beb96a55fcc64f070
Sandhya <144099442+panditsandhya@users.noreply.github.com>
2023-09-05 12:53:05 +0000

    data types

---
+++ 01_basics/02_data_types.js
@@ -0,0 +1,32 @@
+"use strict";// treat all js code as newer version
+
+//alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 + 
+    3) // code readability should be high
+
+let name = "sandhya"
+let age = 20
+let isLoggedIn = false
+
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value(empty value)
+//undefined => values are not define 
+//symbol => unique
+
+
+//object
+
+// console.log(typeof null) 
+// => type null is object
+
+// console.log(typeof undefined) 
+// => type undefined is undefined
+
+
+
+
+
