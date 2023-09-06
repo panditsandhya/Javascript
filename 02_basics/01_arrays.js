b6bfed080cac1993ea16817928abf74565df067c
Sandhya <144099442+panditsandhya@users.noreply.github.com>
2023-09-06 16:59:54 +0000

    arrays

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,39 @@
+//array
+
+const myArr = [0, 1, 2, 3, 4, 5]
+// console.log(myArr[1]);
+
+const myArr2 = new Array(1, 2, 3, 4)
+// console.log(myArr2[0]);
+
+//Array Methods
+// myArr.push(7);
+// myArr.pop();
+
+// myArr.unshift(2)
+// myArr.shift()
+
+// console.log(myArr.includes(8))
+// console.log(myArr.indexOf(0))
+
+// const newArr = myArr.join()
+// console.log(newArr);
+// console.log(typeof newArr);
+// console.log(myArr)
+
+//Slice and splice
+
+console.log("A", myArr);
+
+const myn1 = myArr.slice(1,3)
+
+ console.log(myn1);
+
+const myn2 = myArr.splice(1,3)
+console.log(myn2);
+
+
+
+
+
+
