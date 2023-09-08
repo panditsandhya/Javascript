1abdfbf45501098d3c6b77b60bebc9345207f8dd
Sandhya <144099442+panditsandhya@users.noreply.github.com>
2023-09-08 08:47:20 +0000

    switch case

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,48 @@
+// const month = "3"
+// const month = 3
+
+// switch (month) {
+//     case 1:
+//         console.log("Jan");
+//         break;
+        
+//     case 2:
+//         console.log("Feb");
+//         break;
+//     case 3:
+//         console.log("Mar");
+//         break;
+//     case 4:
+//         console.log("Apr");
+//         break;
+//     case 5:
+//         console.log("may");
+//         break;
+//     case 6:
+//         console.log("Jun");
+//         break;
+//     default:
+//         console.log("Default case match");
+//         break;
+// }
+
+// const month = "Jan"
+
+// switch (month) {
+//     case "Jan":
+//         console.log("Jan");        
+//         break;
+//     case "Feb":
+//         console.log("Feb");        
+//         break;
+//     case "March":
+//         console.log("March");        
+//         break;
+//     case "April":
+//         console.log("April");        
+//         break;
+
+//     default:
+//         console.log("Default value match");
+//         break;
+// }
