c84304a8c22b82a64ca41faf11fef403cf3019fa
Sandhya <144099442+panditsandhya@users.noreply.github.com>
2023-09-08 14:47:40 +0000

    for loop

---
+++ 05_iterations/one.js
@@ -0,0 +1,50 @@
+//for
+
+for (let i = 1; i <= 10; i++) {
+    const element = i;
+    if (element == 5){
+        // console.log("5 is best number");
+    }
+    // console.log(element);
+    
+}
+
+// console.log(element);
+
+for (let i = 1; i <= 10; i++) {
+   //console.log(`Outer loop value: ${i}`);
+   for (let j = 1; j <=10; j++) {
+    //console.log(`Inner loop value ${j} and outer loop value ${i}`);
+    //console.log( i + '*' + j + ' = ' + i*j );
+ }
+    
+}
+
+let myArr = ["BMW", "Safari", "Suzuki"]
+//console.log(myArr.length);
+
+for (let index = 0; index < myArr.length; index++) {
+    const element = myArr[index];
+    //console.log(element);
+    
+}
+
+//break and continue
+
+// for (let index = 1; index <= 20; index++) {
+//     if(index == 5){
+//         console.log(`Detected 5`);
+//         break;
+//     }
+
+//     console.log(`value of i is ${index}`);
+// }
+
+for (let index = 1; index <= 20; index++) {
+    if(index == 5){
+        console.log(`Detected 5`);
+        continue;
+    }
+
+    console.log(`value of i is ${index}`);
+}
