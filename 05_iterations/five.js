ec66be6ba1632dd4db10d140fb33d0027a9bf583
Sandhya <144099442+panditsandhya@users.noreply.github.com>
2023-09-09 02:22:05 +0000

    forEach loop done

--- 05_iterations/five.js
+++ 05_iterations/five.js
@@ -2,6 +2,7 @@
 
 const coding = ["js", "py", "cpp", "java"]
 
+// callback function : - without name
 // coding.forEach( function (val){
 //     console.log(val);
 // })
@@ -14,5 +15,33 @@ function printMe(item){
     console.log(item);
 }
 
-coding.forEach(printMe)
+//coding.forEach(printMe)
+
+coding.forEach( ( item, index, arr) => {
+    //console.log(item, index, arr);
+})
+
+const myCoding = [
+    {
+        languageName: "javascript",
+        languageFileName: "js"
+    },
+
+    {
+        languageName: "java",
+        languageFileName: "java"
+    },
+
+    {
+        languageName: "python",
+        languageFileName: "python"
+    }
+]
+
+myCoding.forEach( (item) => {
+    console.log(item.languageName);
+})
+
+
+
 
